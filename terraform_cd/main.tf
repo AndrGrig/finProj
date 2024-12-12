db0581a4b48ac24dfdfd2aa2e9737ead6dbdc81e
andrey <andreygrigorenko@gmail.com>
2024-12-12 22:21:07 +0200

    commit 15

--- terraform_cd/main.tf
+++ terraform_cd/main.tf
@@ -41,11 +41,52 @@ resource "aws_route_table_association" "main" {
   route_table_id = aws_route_table.main.id
 }
 
+resource "aws_security_group" "k8s_sg" {
+  name        = "k8s-security-group"
+  description = "Security group for K8s EC2 instance"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "k8s-security-group"
+  }
+}
 resource "aws_instance" "k8s_node" {
   ami           = data.aws_ami.latest_amazon_linux.id
   instance_type = var.instance_type
   key_name      = var.key_name
   subnet_id     = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.k8s_sg.id]
 
   tags = {
     Name = "K8s-EC2"
