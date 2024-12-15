2434276fdb19b2319e997a0d995db7163d4f83bb
andrey <andreygrigorenko@gmail.com>
2024-12-15 23:45:56 +0200

    commit 11

--- App/app/s3_utils.py
+++ App/app/s3_utils.py
@@ -9,8 +9,30 @@ def upload_to_s3(file):
     bucket = os.getenv('S3_BUCKET', 'djans-photos-bucket')
     s3.upload_fileobj(file, bucket, file.filename)
 
+# def list_photos():
+#     s3 = get_s3_client()
+#     bucket = os.getenv('S3_BUCKET', 'djans-photos-bucket')
+#     response = s3.list_objects_v2(Bucket=bucket)
+#     return [item['Key'] for item in response.get('Contents', [])]
+
+
 def list_photos():
     s3 = get_s3_client()
     bucket = os.getenv('S3_BUCKET', 'djans-photos-bucket')
     response = s3.list_objects_v2(Bucket=bucket)
-    return [item['Key'] for item in response.get('Contents', [])]
+    
+    photos = []
+    for item in response.get('Contents', []):
+        presigned_url = s3.generate_presigned_url('get_object',
+            Params={
+                'Bucket': bucket,
+                'Key': item['Key']
+            },
+            ExpiresIn=3600  # URL expires in 1 hour
+        )
+        photos.append({
+            'key': item['Key'],
+            'url': presigned_url
+        })
+    
+    return photos
