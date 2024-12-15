afdcd7cbb64fab2bea7f5add0a603c4e3c2be5b4
andrey <andreygrigorenko@gmail.com>
2024-12-15 22:51:40 +0200

    secret_key

--- App/app/__init__.py
+++ App/app/__init__.py
@@ -7,7 +7,8 @@ def create_app():
         'DATABASE_URL', 'mysql+pymysql://user:mysecurepassword@db_address/photoDB'
     )
     app.config['S3_BUCKET'] = os.getenv('S3_BUCKET', 'djans-photos-bucket')
-    app.secret_key = os.getenv('FLASK_SECRET_KEY', 'your-dev-secret-key123456')
+    # app.secret_key = os.getenv('FLASK_SECRET_KEY', 'your-dev-secret-key123456')
+    app.config['SECRET_KEY'] = os.urandom(24)
 
     from .routes import main
     app.register_blueprint(main)
